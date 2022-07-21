63649f0a49313950e777f3e7cde2de0750377785
Alexander KIRILOV <aleksandar.kirilov@proxym.fr>
2022-07-21 10:17:57 +0200

    working with requirejs

---
+++ src/app.js
@@ -0,0 +1,11 @@
+define(() => {
+  return class MyClass {
+    constructor() {
+      console.log('MY CLASS');
+    }
+
+    calc(a, b) {
+      return a + b;
+    }
+  }
+})
---
+++ test/test.spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+  describe('just checking', function() {
+    console.log('********* TEST STARTED *****');
+
+    let app = require('src/app')
+
+    it('works for app', function() {
+      console.log('App is', app);
+    });
+  });
+});
+
